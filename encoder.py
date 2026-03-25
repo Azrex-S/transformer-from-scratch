d789ebbd5426c648c4ec14fc3dbf65da8dc20652
Azrex-S <azrex223@gmail.com>
2026-03-25 15:32:46 +0530

    fixed some syntax

--- encoder.py
+++ encoder.py
@@ -15,19 +15,21 @@ def scaled_dot_product(q,k,v,mask=None):
 
 class MultiHeadedAttention(nn.Module):
     def __init__(self,d_model,num_heads):
-        super.__init__()
+        super().__init__()
         self.d_model=d_model
         self.num_heads=num_heads
         self.qkv_layer=nn.Linear(d_model,3*d_model)
         self.linear_layer=nn.Linear(d_model,d_model)
-    def foward(self,x,mask=None):
+    def forward(self,x,mask=None):
         batch_size,max_seq_length,d_model=x.size()
         qkv=self.qkv_layer(x)
         qkv=qkv.reshape(batch_size,max_seq_length,self.num_heads,3*self.head_dim)
         qkv=qkv.permute(0,2,1,3)
-        q,k,v=qkv.chunk(3,dim=1)
+        q,k,v=qkv.chunk(3,dim=-1)
         values,attention= scaled_dot_product(q,k,v,mask)
         values=values.reshape(batch_size,max_seq_length,self.num_heads)
+        out=self.linear_layer(values)
+        return out
 
 
 
